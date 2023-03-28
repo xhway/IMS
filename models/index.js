06ee70bbf888f53e9346669570348bd35454db3f
Reggie Ulysse <reggieulysse@gmail.com>
2023-03-27 21:28:41 -0400

    updated homeRoutes and created a UserRoutes

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@ const Pants = require('./Pants');
 const Shirt = require('./Shirt');
 const Shoes = require('./Shoes');
 const Tie = require('./Tie');
+const User = require('./User')
 
 
-module.exports = { Jacket, Pants, Shirt, Shoes, Tie };
+module.exports = { Jacket, Pants, Shirt, Shoes, Tie , User};
